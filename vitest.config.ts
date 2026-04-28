3038e0a151ed03edead60be4b09ac9abdeac397b
RSI Platform <platform@rsi2.digify.no>
2026-04-28 07:44:40 +0200

    WIP: rsi session checkpoint

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,11 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
-  test: { environment: 'nuxt' },
+  test: {
+    environment: 'nuxt',
+    poolOptions: {
+      forks: {
+        maxForks: 2,
+      },
+    },
+  },
 })
